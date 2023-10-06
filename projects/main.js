0c47f23f7bfc19cf306865481baa699714ca5343
Pradip <pd710109@gmail.com>
2023-10-06 16:50:28 +0530

    adding new  code

--- projects/main.js
+++ projects/main.js
@@ -25,3 +25,47 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+// food and dite palne site
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// faq
+
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(question => {
+  question.addEventListener('click', () => {
+    question.classList.toggle('active');
+  });
+});
+
+
+// menu hide and show
+
+function myFunction() {
+  var x = document.getElementsByTagName("nav");
+  if (x.className === "btn") {
+    x.className += " responsive";
+  } else {
+    x.className = "btn";
+  }
+}
